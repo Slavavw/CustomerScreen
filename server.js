0810033a545530ceca8eac4694ab9de07e48e3d1
Slavavw <slavavw1978@gmail.com>
2026-04-03 17:47:22 +0300

    изменил формирование bakground

--- server.js
+++ server.js
@@ -395,7 +395,7 @@ server.on("request", (request, response) => {
       streamRead.on("end", async () => {
         await response.writeHead(200, { "content-type": "text/html; chatset=utf-8" });
         let styleBkground =
-          '<style type="text/css">body{background-image:url(/images/background/background.jpg);background-repeat: no-repeat;background-position: left top;background-attachment: fixed;background-size: cover;}</style>';
+          '<style type="text/css">body{background-image:url(/images/background/background.jpg);background-repeat: no-repeat;background-origin: border-box; background-position: center center;background-attachment: fixed;background-size: 100vw 100vh;}</style>';
         stat(path.join(__dirname, "images", "background", "background.jpg"))
           .catch(() => {
             styleBkground = '<style type="text/css">body{background-image:url(/images/background_main.jpg);}</style>';
